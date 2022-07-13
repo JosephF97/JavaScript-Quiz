f3c672a1422ec703bdae8361b1b1b3a7b4651377
JosephF97 <107017450+JosephF97@users.noreply.github.com>
2022-07-12 18:34:09 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,96 @@
+// TODO: store all questions here
+const questions = [
+    {
+        question: "Inside which HTML element do we put Javascript in?",
+        answers: {
+            A: "<javascript>",
+            B: "<js>",
+            C: "<scripting>",
+            D: "<script>"
+        },
+        correctAnswer: "D"
+    },
+    {
+        question: "An external Javascript file must contain a <script> tag.",
+        answers: {
+            A: "True",
+            B: "False"
+        },
+        correctAnswer: "B"
+    },
+    {
+        question: "How do you create a function in Javascript?",
+        answers: {
+            A: "function myfunction()",
+            B: "function = myFunction()",
+            C: "function:myFunction()" 
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Where is the correct place to insert JavaScript?",
+        answers: {
+            A: "<head>",
+            B: "<body>",
+            C: "Both"
+        },
+        correctAnswer: "C"
+    },
+    {
+        question: "What is the correct syntax when referring to an external script called 'script.js'?",
+        answers: {
+            A: "<script src='script.js'>",
+            B: "<script name ='script.js'>",
+            C: "<script href = 'script.js'>",
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "How do you add a comment in JavaScript?",
+        answers:{
+            A: "//comment",
+            B: "'comment",
+            C: "<!--comment-->",
+        },
+        correctAnswer: "A",
+    },
+    {
+        question: "JavaScript is the same as Jave?",
+        answers: {
+            A: "True",
+            B: "False",
+        },
+        correctAnswer: "A",
+    },
+    {
+        question: "What is the correct way to write an array in JavaScript?",
+        answers: {
+            A: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue') ",
+            B: "var colors = 'red', 'green', 'blue'",
+            C: "var colors = ['red', 'green', 'blue']",
+            D: "var colors = (1:'red', 2:'green',3:'blue')",
+        },
+        correctAnswer: "C"
+    },
+    {
+        question: "How do you write in an alert box?",
+        answers: {
+            A: "alert()",
+            B: "msg",
+            C: "alertBox()",
+            D: "msgbox()"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Is JavaScript case-sensitive?",
+        answers: {
+            A: "True",
+            B: "False",
+        },
+        correctAnswer: "A"
+    },
+
+];
+
+// NOTE: hint - can use an array that contains objects
